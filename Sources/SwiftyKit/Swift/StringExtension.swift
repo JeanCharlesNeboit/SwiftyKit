625930dcb7a1e41594b7ec5471fb436327191171
Jean-Charles Neboit <jean_charles.neboit@icloud.com>
2021-04-01 12:21:21 +0200

    Add String extension

---
+++ Sources/SwiftyKit/Swift/StringExtension.swift
@@ -0,0 +1,24 @@
+//
+//  StringExtension.swift
+//  Speech
+//
+//  Created by Jean-Charles Neboit on 31/03/2021.
+//
+
+import Foundation
+
+public extension Optional where Wrapped == String {
+    var strongValue: String {
+        return self ?? ""
+    }
+}
+
+public extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).capitalized + dropFirst()
+    }
+    
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
