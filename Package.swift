c3cb0b1452fb557cab403c64be72faa631a55667
Jean-Charles Neboit <jean_charles.neboit@icloud.com>
2021-04-23 17:04:17 +0200

    Add macOS platform

--- Package.swift
+++ Package.swift
@@ -3,9 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyKit",
-    platforms: [
-        .iOS(.v11)
-    ],
+    platforms: [.iOS(.v11), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
