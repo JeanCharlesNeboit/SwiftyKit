0a07f1dad8c271ba0c523162c92b9051dc37a812
Jean-Charles Neboit <jean_charles.neboit@icloud.com>
2021-09-08 23:34:01 +0200

    Add currentWindow in UIWindowExtension

--- Sources/SwiftyKit/UIKit/UIWindowExtension.swift
+++ Sources/SwiftyKit/UIKit/UIWindowExtension.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 extension UIWindow {
+    public static var currentWindow: UIWindow? {
+        if #available(iOS 13.0, *) {
+            return UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .compactMap({$0 as? UIWindowScene})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first
+        } else {
+            return UIApplication.shared.keyWindow
+        }
+    }
+    
     public func set(rootViewController vc: UIViewController, animated: Bool = false, animationOptions: UIView.AnimationOptions = .transitionCrossDissolve) {
          if animated {
             let animations = { [self] in
