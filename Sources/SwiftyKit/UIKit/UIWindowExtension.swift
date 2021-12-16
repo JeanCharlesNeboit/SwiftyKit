c091af0a2bbbd5ce73acc085b2fce0e43346f765
Jean-Charles Neboit <jean_charles.neboit@icloud.com>
2021-12-16 11:29:48 +0100

    Clean code in UIWindowExtension.swift

--- Sources/SwiftyKit/UIKit/UIWindowExtension.swift
+++ Sources/SwiftyKit/UIKit/UIWindowExtension.swift
@@ -12,10 +12,10 @@ extension UIWindow {
     public static var currentWindow: UIWindow? {
         if #available(iOS 13.0, *) {
             return UIApplication.shared.connectedScenes
-                .filter({$0.activationState == .foregroundActive})
-                .compactMap({$0 as? UIWindowScene})
+                .filter { $0.activationState == .foregroundActive }
+                .compactMap { $0 as? UIWindowScene }
                 .first?.windows
-                .filter({$0.isKeyWindow}).first
+                .filter { $0.isKeyWindow }.first
         } else {
             return UIApplication.shared.keyWindow
         }
