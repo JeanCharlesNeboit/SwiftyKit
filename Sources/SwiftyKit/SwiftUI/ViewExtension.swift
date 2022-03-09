7a29309f1e54214186f09ac151b464b07089c04d
Jean-Charles Neboit <jean_charles.neboit@icloud.com>
2022-03-10 00:06:22 +0100

    Add ViewExtension.swift

---
+++ Sources/SwiftyKit/SwiftUI/ViewExtension.swift
@@ -0,0 +1,26 @@
+//
+//  ViewExtension.swift
+//  
+//
+//  Created by Jean-Charles Neboit on 14/02/2022.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+@available(iOS 13.0, *)
+public struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    public func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
